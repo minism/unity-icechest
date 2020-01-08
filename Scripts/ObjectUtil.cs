f969ce65d36aa2f64e7bac9081ef4192816a8b9a
josh <joshbothun@gmail.com>
2020-01-07 16:38:56 -0800

    Expand ObjectUtil.

--- Scripts/ObjectUtil.cs
+++ Scripts/ObjectUtil.cs
@@ -3,12 +3,26 @@
 namespace Ice {
   public static class ObjectUtil {
     public static void DestroyChildrenImmediate(GameObject parent) {
-      GameObject[] children = new GameObject[parent.transform.childCount];
-      for (int i = 0; i < parent.transform.childCount; i++) {
-        children[i] = parent.transform.GetChild(i).gameObject;
+      DestroyChildrenImmediate(parent.transform);
+    }
+
+    public static void DestroyChildrenImmediate(Transform parent) {
+      GameObject[] children = new GameObject[parent.childCount];
+      for (int i = 0; i < parent.childCount; i++) {
+        children[i] = parent.GetChild(i).gameObject;
       }
       foreach (var child in children) {
         Object.DestroyImmediate(child.gameObject);
+      }
+    }
+
+    public static void DestroyChildren (GameObject parent) {
+      DestroyChildren(parent.transform);
+    }
+
+    public static void DestroyChildren (Transform parent) {
+      foreach (Transform child in parent) {
+        Object.Destroy(child.gameObject);
       }
     }
   }
