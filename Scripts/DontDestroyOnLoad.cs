d278ccba7172b3b12aa6c17a68630ea2f69a85d8
josh <joshbothun@gmail.com>
2020-01-27 14:31:30 -0800

    Add DDOL

---
+++ Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Ice {
+  public class DontDestroyOnLoad : MonoBehaviour {
+    private void Awake() {
+      DontDestroyOnLoad(gameObject);
+    }
+  }
+} // namespace Ice
