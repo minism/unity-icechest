a184340d7fe6111f0518cf467f1c5a91befc673b
josh <joshbothun@gmail.com>
2020-01-27 14:31:16 -0800

    Add more objectutil methodS

--- Scripts/ObjectUtil.cs
+++ Scripts/ObjectUtil.cs
@@ -11,5 +11,19 @@ namespace Ice {
         Object.DestroyImmediate(child.gameObject);
       }
     }
+
+    public static void DestroyChildren(GameObject parent) {
+      GameObject[] children = new GameObject[parent.transform.childCount];
+      for (int i = 0; i < parent.transform.childCount; i++) {
+        children[i] = parent.transform.GetChild(i).gameObject;
+      }
+      foreach (var child in children) {
+        Object.Destroy(child.gameObject);
+      }
+    }
+
+    public static void DestroyChildren(Transform parent) {
+      DestroyChildren(parent.gameObject);
+    }
   }
 } // namespace Ice
