4ac4f03ac8e79c515a0fa0aa139c26e2de46702f
josh <joshbothun@gmail.com>
2020-01-28 15:41:24 -0800

    Improve the asset preprocessor to allow auto-slicing of sprites

--- Editor/PresetDirectoryProcessor.cs
+++ Editor/PresetDirectoryProcessor.cs
@@ -1,10 +1,16 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Presets;
+using UnityEngine;
 
 namespace Ice {
 
+  // Uses preset assets in the current or parent directory for automatically applying
+  // asset settings.
   public class PresetDirectoryProcessor : AssetPostprocessor {
+    private SpriteSheetSettings spriteSheetSettings;
+
     void OnPreprocessAsset() {
       // Make sure we are applying presets the first time an asset is imported.
       if (assetImporter.importSettingsMissing) {
@@ -20,6 +26,7 @@ namespace Ice {
               // Load the Preset and try to apply it to the importer.
               var preset = AssetDatabase.LoadAssetAtPath<Preset>(presetPath);
               if (preset.ApplyTo(assetImporter))
+                MaybeLoadSpriteSheetSettings(Path.GetDirectoryName(presetPath));
                 return;
             }
           }
@@ -27,6 +34,42 @@ namespace Ice {
           // Try again in the parent folder.
           path = Path.GetDirectoryName(path);
         }
+      }
+    }
+
+    void OnPostprocessTexture(Texture2D texture) {
+      if (spriteSheetSettings == null) {
+        return;
+      }
+      var textureImporter = assetImporter as TextureImporter;
+      if (textureImporter.textureType != TextureImporterType.Sprite) {
+        return;
+      }
+
+      var w = spriteSheetSettings.cellWidth;
+      var h = spriteSheetSettings.cellHeight;
+      int cols = texture.width / w;
+      int rows = texture.height / h;
+      List<SpriteMetaData> metadatas = new List<SpriteMetaData>();
+      for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+          var metadata = new SpriteMetaData();
+          metadata.rect = new Rect(c * w, r * h, w, h);
+          metadata.name = $"{c}-{r}";
+          metadatas.Add(metadata);
+        }
+      }
+
+      Debug.Log($"Ice: Automatically sliced sprite {assetPath} according to settings.");
+      ((TextureImporter)assetImporter).spritesheet = metadatas.ToArray();
+      AssetDatabase.Refresh();
+    }
+
+    void MaybeLoadSpriteSheetSettings(string dirname) {
+      var guids = AssetDatabase.FindAssets("t:SpriteSheetSettings", new[] { dirname });
+      if (guids.Length > 0) {
+        spriteSheetSettings = AssetDatabase.LoadAssetAtPath<SpriteSheetSettings>(
+            AssetDatabase.GUIDToAssetPath(guids[0]));
       }
     }
   }
---
+++ Editor/SpriteSheetSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// If this is present in a directory with sprites, sprites in the same directory will automatically
+/// be sliced according to these rules.
+[CreateAssetMenu()]
+public class SpriteSheetSettings : ScriptableObject {
+  public int cellWidth;
+  public int cellHeight;
+}
