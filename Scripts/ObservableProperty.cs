f0b72097c949a1c569b1bfb6576549e958c0ec1e
josh <joshbothun@gmail.com>
2020-03-28 17:50:49 -0700

    Observable

---
+++ Scripts/ObservableProperty.cs
@@ -0,0 +1,27 @@
+﻿namespace Ice {
+
+  public class ObservableProperty<T> {
+    private T _value;
+    public T Value {
+      get { return _value; }
+      set {
+        if (!value.Equals(Value)) {
+          Value = value;
+          OnChange?.Invoke(Value);
+        }
+      }
+    }
+
+    public delegate void ChangeEvent(T data);
+    public event ChangeEvent OnChange;
+
+    public ObservableProperty(T initialValue) {
+      Value = initialValue;
+    }
+
+    public static implicit operator T(ObservableProperty<T> p) {
+      return p.Value;
+    }
+  }
+
+} // namespace Ice
