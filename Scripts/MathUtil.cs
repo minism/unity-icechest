0c142e847939b0f700ea02bbf892edc06ea0aa2c
josh <joshbothun@gmail.com>
2020-04-04 16:51:21 -0700

    Add a math util

---
+++ Scripts/MathUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Ice {
+  public static class MathUtil {
+    public static int RoundToMultiple(int value, int multiple) {
+      return (value / multiple) * multiple;
+    }
+
+    public static int RoundToMultiple(float value, int multiple) {
+      return RoundToMultiple((int)value, multiple);
+    }
+  }
+}
